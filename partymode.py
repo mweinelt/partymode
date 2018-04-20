31205877f6ce0d5b1ee938210f58052afb342b06
Diula <j.pirnat@icloud.com>
2018-04-21 00:59:48 +0200

    Automatisch Abschaltung
    Fixes #5

--- partymode.py
+++ partymode.py
@@ -2,6 +2,7 @@
 
 import os
 from subprocess import run
+from datetime import date
 
 import paho.mqtt.client as mqtt
 import pyotp
@@ -20,9 +21,11 @@ else:
 
 partymode_totp = pyotp.TOTP(partymode_totp_secret)
 partymode_enabled = False
+partymode_enabled_date = None
 topic_bell = "w17/door/bell/state"
 topic_party_set = "w17/door/partymode/enabled/set"
 topic_party_state = "w17/door/partymode/enabled"
+topic_door_lock_state = "w17/door/lock/state"
 
 # print provisioning qrcode to terminal
 print("QRCode, usable with TOTP Application:")
@@ -30,30 +33,66 @@ qr = QRCode()
 qr.add_data(partymode_totp.provisioning_uri('enable key', 'w17/door/partymode'))
 qr.print_tty()
 
-#connection with mqtt, to receive the bellstate
+# connection with mqtt broker, subscribe to updates for several states
 def on_connect(client, userdata, flags, rc):
     print("Verbunden mit mqtt.")
-    client.subscribe(topic_bell)
-    client.subscribe(topic_party_set)
+    client.subscribe([
+        (topic_bell, 0),  # Does the bell ring?
+        (topic_party_set, 0),  # Does somebody try to en/disable the partymode?
+        (topic_door_lock_state, 0)  # Is the door locked?
+    ])
 
-#working with the message which has been delivered from the doorbell, running the summer
+# receive updates from mqtt broker
 def on_message(client, userdata, msg):
     global partymode_enabled
+
+    # Read topic and payload from message
+    # payload is the value of the message
+    # topic describes what has changed
     payload = msg.payload.decode()
     topic = msg.topic
-    #print(topic, payload)
+
+    # Decision, which codepath will be used, depending on the topic
     if topic == topic_party_set:
-        partymode_enabled = payload == partymode_totp.now()
-        if partymode_enabled:
-            print("Let's get ready to rumble!")
-            client.publish(topic_party_state, payload=1)
-        else:
-            print("Bell functions as usually.")
-            client.publish(topic_party_state, payload=0)
-    if topic == topic_bell:
-        if partymode_enabled and payload == "1":
+        switch_partymode(
+            enable=payload == partymode_totp.now(),
+            client=client
+        )
+    elif topic == topic_bell:
+        if not partymode_enabled:
+            return
+
+        if payload == "1":
+            if date.today() > partymode_enabled_date:
+                switch_partymode(
+                    enable=False,
+                    client=client
+                )
+                print("The day has changed. Therefore, the door now works as "
+                      "usual and the partymode has been disabled.")
+                return
+
             run("/door/wrapper_summer")
             print("Auf!")
+    elif topic == topic_door_lock_state:
+        # Door is locked.
+        if payload == "1":
+            switch_partymode(
+                enable=False,
+                client=client
+            )
+
+def switch_partymode(enable, client):
+    global partymode_enabled, partymode_enabled_date
+    partymode_enabled = enable
+
+    if enable:
+        print("Let's get ready to rumble!")
+        client.publish(topic_party_state, payload=1)
+        partymode_enabled_date = date.today()
+    else:
+        print("Bell functions as usually.")
+        client.publish(topic_party_state, payload=0)
 
 client = mqtt.Client()
 
